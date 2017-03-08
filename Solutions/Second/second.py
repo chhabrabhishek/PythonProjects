fb1bbbe62dbbdc090eb3fe5d2a9c2bcafc6d4dfb
chhabrabhishek <abhishekchhabra107@gmail.com>
2017-03-08 21:38:06 +0530

    Bro just check it once

--- Solutions/Second/second.py
+++ Solutions/Second/second.py
@@ -1,40 +1,43 @@
-#!/usr/bin/python
 from PIL import Image
-n=int(raw_input("Enter the distance between 2 consecutive files: "))
-m=2*n
 X=0
 Y=0
+n=int(raw_input("Enter the distance between 2 consecutive lines"))
+w=int(512/(2*n))
 try:
-	im=Image.new('RGB',(512,512))
+	im=Image.new('RGB',(513,513))
 except:
 	print"Unable to load image"
 pixels=im.load()
-while(X<=(512-n)):
-	for i in range(0,m):
+for i in range(0,w):
+	for j in range(0,n):
 		X=X+1
 		Y=Y+1
 	A=X
-	B=0
-	while(A!=0):
+	B=Y
+	while(B!=0):
 		pixels[A,B]=(255,255,255)
+		A=A+1
+		B=B-1
+	A=X
+	B=Y
+	while(A!=0):
 		A=A-1
 		B=B+1
-	m=n
-
-X=512
-Y=512
-m=2*n
-while(X>=n):
-	
-	X -= m
-	Y -= m
-
+		pixels[A,B]=(255,255,255)
+for k in range(0,w):
+	for l in range(0,n):
+		X=X+1
+		Y=Y+1
 	A=X
-	B=511
+	B=Y
 	while(A!=512):
 		pixels[A,B]=(255,255,255)
 		A=A+1
 		B=B-1
-
-	m=n
+	A=X
+	B=Y
+	while(B!=512):
+		A=A-1
+		B=B+1
+		pixels[A,B]=(255,255,255)
 im.show()
