1f39da3f2a9e2033b97abe6254c1a5657ae070d1
Kilari teja <kilariteja9@gmail.com>
2017-03-09 01:31:28 +0530

    Updated Accepted Solution

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # PythonProjects
 
-## Instructions
-Please tick off the Question that has been solved in the Challenges List, By Adding a "[S]" After the Question Number.
+##Accepted Solutions
+1, 2
--- Solutions/Second/mine.py
+++ Solutions/Second/mine.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import Image
 
-dbLines, lImg = int(raw_input("Enter The Distance Between Lines: "))*(2**0.5), Image.new("RGB", (512, 512))
-for 
+dbLineSpace, lImg = int(raw_input("Enter The Distance Between Lines: "))*(2**0.5), Image.new("RGB", (512, 512))
+for lPoint in xrange(int(512/dbLineSpace)):
+
+	
